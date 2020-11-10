e3d8079bded720c3819a797186e6c63738068d50
ncbecker <nbecker.ac@gmail.com>
2020-11-10 14:56:08 +0100

    Add object with multiple passwords and show on correct input

--- app.js
+++ app.js
@@ -5,19 +5,30 @@ const inquirer = require("inquirer");
 
 console.log("PW-Manager");
 
-const args = process.argv.slice(2);
+// const args = process.argv.slice(2);
 
-console.log(args);
+// console.log(args);
 
-const passwordName = args[0];
+// const passwordName = args[0];
 
 const secretMasterPassword = "helloworld";
 
+const passwordSafe = {
+  wifi: "password123",
+  github: "helloworld",
+  vercel: "niceapp",
+};
+
 const questions = [
   {
-    type: "input",
+    type: "password",
     name: "masterPassword",
     message: "What is the super secret master password?",
+  },
+  {
+    type: "input",
+    name: "passwordName",
+    message: "Which password did you forget?",
   },
 ];
 
@@ -25,7 +36,7 @@ const questions = [
 
 async function validateAccess() {
   // await object "answers" is destructured; note: to access object properties you can use answers["name"] or answers.name
-  const { masterPassword } = await inquirer.prompt(questions);
+  const { masterPassword, passwordName } = await inquirer.prompt(questions);
 
   if (masterPassword !== secretMasterPassword) {
     console.error(chalk.red("You are not welcome here! 👿"));
@@ -33,11 +44,11 @@ async function validateAccess() {
     // return is needed since function is async; otherwise the function wouldn't be called immediately
     return;
   }
-
-  if (passwordName === "Nico") {
-    console.log(chalk.green("Password is honigblume"));
+  const passwordSafeKeys = Object.keys(passwordSafe);
+  if (passwordSafeKeys.includes(passwordName)) {
+    console.log(chalk.green(passwordSafe[passwordName]));
   } else {
-    console.log(chalk.yellow("Unknown password"));
+    console.log(chalk.yellow("Does not exist in database!"));
   }
 }
 
