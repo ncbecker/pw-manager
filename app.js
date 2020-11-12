5260727deee8bc43ac889fd4894de93e9f0af5e6
ncbecker <nbecker.ac@gmail.com>
2020-11-12 11:04:18 +0100

    Create separate component for questions

--- app.js
+++ app.js
@@ -1,75 +1,47 @@
-// require similar to import
-
-const chalk = require("chalk");
-const inquirer = require("inquirer");
 const fs = require("fs").promises;
 const CryptoJS = require("crypto-js");
+const chalk = require("chalk");
+const {
+  askForMasterPassword,
+  askGetOrSave,
+  askPasswordName,
+  askNewTitle,
+  askNewPassword,
+} = require("./lib/questions");
 
 console.log(chalk.magenta("PW-Manager"));
 
 const secretMasterPassword = "helloworld";
 
-const questionMasterPassword = [
-  {
-    // type password for hidden input
-    type: "password",
-    name: "masterPassword",
-    message: "What is the super secret master password?",
-  },
-];
-
-const questionPasswordName = [
-  {
-    type: "input",
-    name: "passwordName",
-    message: "Which password did you forget?",
-  },
-];
-
-const questionGetOrSave = [
-  {
-    type: "list",
-    name: "answerGetOrSave",
-    message: "Please choose: SAVE or GET a password?",
-    choices: ["SAVE", "GET"],
-  },
-];
-
-// instead of ".then" we can use async function and await
-
-async function validateAccess() {
-  // await object "answers" is destructured; note: to access object properties you can use answers["name"] or answers.name
-  const { masterPassword } = await inquirer.prompt(questionMasterPassword);
+async function run() {
+  const masterPassword = await askForMasterPassword();
 
   if (masterPassword !== secretMasterPassword) {
     console.error(chalk.red("You are not welcome here! 👿"));
-    validateAccess();
-    // return is needed since function is async; otherwise the function wouldn't be called immediately
-    return;
+    return run();
   }
 
-  const { answerGetOrSave } = await inquirer.prompt(questionGetOrSave);
+  const answerSaveOrGet = await askGetOrSave();
 
   const passwordSafeJSON = await fs.readFile("./db.json", "utf8");
 
   const passwordSafe = JSON.parse(passwordSafeJSON);
 
-  if (answerGetOrSave === "GET") {
-    const { passwordName } = await inquirer.prompt(questionPasswordName);
+  if (answerSaveOrGet === "GET") {
+    const answerPasswordName = await askPasswordName();
 
     const passwordSafeKeys = Object.keys(passwordSafe);
 
-    // Alternative: if(passwordSafe[passwordName]); returns true if value exists; Object.keys not necessary
-    if (passwordSafeKeys.includes(passwordName)) {
+    if (passwordSafeKeys.includes(answerPasswordName)) {
       const decryptPassword = decryptData(
-        passwordSafe[passwordName],
+        passwordSafe[answerPasswordName],
         secretMasterPassword
       );
       console.log(chalk.green(decryptPassword));
     } else {
       console.log(chalk.yellow("Does not exist in database!"));
     }
-  } else if (answerGetOrSave === "SAVE") {
+  } else if (answerSaveOrGet === "SAVE") {
     const newEntry = await createNewEntry();
     const newPasswordSafe = Object.assign(passwordSafe, newEntry);
     const data = JSON.stringify(newPasswordSafe);
@@ -82,32 +54,16 @@ async function validateAccess() {
   }
 }
 
-// Question prompt for new entries
-
-const questionNewTitle = [
-  {
-    type: "input",
-    name: "answerTitle",
-    message: "Title?",
-  },
-];
-
-const questionNewPassword = [
-  {
-    type: "password",
-    name: "answerPassword",
-    message: "Password?",
-  },
-];
+// new entry component
 
 async function createNewEntry() {
-  const { answerTitle } = await inquirer.prompt(questionNewTitle);
-  const { answerPassword } = await inquirer.prompt(questionNewPassword);
-  const encryptPassword = encryptData(answerPassword, secretMasterPassword);
-  return { [answerTitle]: encryptPassword };
+  const answerNewTitle = await askNewTitle();
+  const answerNewPassword = await askNewPassword();
+  const encryptPassword = encryptData(answerNewPassword, secretMasterPassword);
+  return { [answerNewTitle]: encryptPassword };
 }
 
-// crypto-js
+// crypto-js component
 
 function encryptData(data, password) {
   return CryptoJS.AES.encrypt(JSON.stringify(data), password).toString();
@@ -117,4 +73,4 @@ function decryptData(data, password) {
   return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
 }
 
-validateAccess();
+run();
