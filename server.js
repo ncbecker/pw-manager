96f583a0738db684e88692e9cac78f0fd86400dc
ncbecker <nbecker.ac@gmail.com>
2020-11-16 14:18:05 +0100

    ✨ Connect to db, get pw by name via express server

---
+++ server.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const chalk = require("chalk");
+const kleur = require("kleur");
+const express = require("express");
+const { connect, getPassword, getAllPasswords } = require("./lib/database");
+
+const app = express();
+const port = 3600;
+
+app.get("/api/passwords/:name", async (request, response) => {
+  const { name } = request.params;
+  const passwordValue = await getPassword(name);
+  response.send(passwordValue);
+});
+
+app.get("/api/passwords/all", async (request, response) => {
+  const passwordNames = await getAllPasswords();
+  response.send(passwordNames);
+});
+
+app.post("/api/passwords", (request, response) => {
+  response.send("Under construction");
+});
+
+async function run() {
+  console.log(kleur.bgYellow(chalk.magenta("PW-Manager")));
+  console.log(chalk.magenta("Connecting to database..."));
+  await connect(process.env.DB_URL, process.env.DB_NAME);
+  console.log(chalk.magenta("Connected to database 🎉"));
+
+  app.listen(port, () => {
+    console.log(`PW-Manager API listening at http://localhost:${port}`);
+  });
+}
+
+run();
