d4510198dc2f58002b09f97f96a0b6b940674ba2
ncbecker <nbecker.ac@gmail.com>
2020-11-18 15:44:40 +0100

    ✨ Add useAsync custom hook and input field

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,31 @@ import logo from "./logo.svg";
 import "./App.css";
 import { getPassword } from "./api/passwords";
 import { useEffect, useState } from "react";
+import useAsync from "./hooks/useAsync";
 
 function App() {
   const [password, setPassword] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [passwordName, setPasswordName] = useState("");
+
+  const { data, loading, error, doFetch } = useAsync(() =>
+    getPassword(passwordName)
+  );
 
   useEffect(() => {
-    try {
-      async function pwFetch() {
-        setLoading(true);
-        const passwordValue = await getPassword("pw4ufork");
-        setPassword(passwordValue);
-      }
-      pwFetch();
-    } catch (error) {
-      console.error(error);
-      setError(error);
-    } finally {
-      setLoading(false);
-    }
+    doFetch();
   }, []);
+
+  function handleChange(event) {
+    setPasswordName(event.target.value);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    doFetch();
+    // const passwordValue = await getPassword(passwordName);
+    // setPassword(passwordValue);
+    setPasswordName("");
+  }
 
   return (
     <div className="App">
@@ -30,7 +34,20 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         {loading && <div>Loading...</div>}
         {error && <div>{error.message}</div>}
-        {password}
+        {data}
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Password Name"
+            name="name"
+            value={passwordName}
+            onChange={handleChange}
+            required
+          />
+          <button type="button" onClick={handleSubmit}>
+            Get Password
+          </button>
+        </form>
       </header>
     </div>
   );
