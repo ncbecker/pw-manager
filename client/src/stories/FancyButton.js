0626b61de9750b905bc88c3ca2783a35ce43a74a
ncbecker <nbecker.ac@gmail.com>
2020-11-21 18:57:03 +0100

    :construction: Add new button component to test storybook

---
+++ client/src/stories/FancyButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import PropTypes from "prop-types";
+import "./button.css";
+
+export const FancyButton = () => {
+  return <button type="button">TEST</button>;
+};
+
+// FancyButton.propTypes = {
+//   label: PropTypes.string.isRequired,
+//   onClick: PropTypes.func,
+// };
+
+// FancyButton.defaultProps = {
+//   onClick: undefined,
+// };
---
+++ client/src/stories/FancyButton.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { FancyButton } from "./Button";
+
+export default {
+  title: "PW-Manager/Button",
+  component: FancyButton,
+};
+
+export const niceButton = () => <FancyButton />;
